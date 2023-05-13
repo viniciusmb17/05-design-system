5f9ce796bbce11b35a5c330796cfb3ee8957eeba
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-05-13 18:09:50 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Components
+
+- [ ] Text
+- [ ] Heading
+- [ ] Box / Card
+- [ ] Button
+- [ ] TextInput
+- [ ] TextArea
+- [ ] Checkbox
+- [ ] Avatar
+- [ ] MultiStep
---
+++ packages/docs/.storybook/preview.js
@@ -0,0 +1,22 @@
+/** @type { import('@storybook/react').Preview } */
+import { themes } from "@storybook/theming";
+
+const preview = {
+  parameters: {
+    backgrounds: {
+      default: "dark",
+    },
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    docs: {
+      theme: themes.dark,
+    },
+  },
+};
+
+export default preview;
