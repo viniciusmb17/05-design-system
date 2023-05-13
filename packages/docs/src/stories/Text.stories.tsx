a4c0dd54b6ca5a3096caa537aa2f02f5ede8fb11
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-05-13 19:05:03 -0300

    feat: Text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto a consectetur unde eveniet eius molestiae deleniti inventore beatae. Eum laborum voluptatum consectetur. Repellendus minima autem praesentium impedit possimus? Earum, natus.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
