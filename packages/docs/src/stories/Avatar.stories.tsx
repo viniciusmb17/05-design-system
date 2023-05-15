54693b70237c069646742e39edfb1e98aff5dd52
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-05-15 10:39:48 -0300

    feat: Avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  tags: ['autodocs'],
+  args: {
+    src: 'https://github.com/viniciusmb17.png',
+    alt: 'Vinicius Barbosa',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
